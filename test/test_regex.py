07de09f0b2e9c89ca492a07d88726cf749da4acb
Esben Sonne <esbensonne@gmail.com>
2014-04-11 10:55:39 +0200

    Added more tests of versions.

--- test/test_regex.py
+++ test/test_regex.py
@@ -5,3 +5,16 @@ from nose.tools import raises
 @raises(InvalidVersionException)
 def test_invalid():
     SemVerPy('1.a.0')
+
+
+def test_regex():
+    SemVerPy('v1.0.0')
+    SemVerPy('V1.0.0')
+
+    SemVerPy('v1.0.0-build')
+    SemVerPy('V1.0.0-build')
+
+
+@raises(InvalidVersionException)
+def test_invalid_build():
+    SemVerPy('v1.0.0-build info')
