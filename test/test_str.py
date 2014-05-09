ddf5d40e2d70bc5ff737dc2ebcd412f1e0e8b5e8
Esben Sonne <esbensonne@gmail.com>
2014-05-09 16:16:00 +0200

    Added shorthand test.

--- test/test_str.py
+++ test/test_str.py
@@ -8,6 +8,12 @@ def test_same():
 
 
 def test_dependency():
-    ver = SemVerPy('1.2')
+    ver = SemVerPy('1.2', dependency=True)
     print(str(ver))
     assert str(ver) == '1.2.x'
+
+
+def test_shorthand():
+    ver = SemVerPy('1.2')
+    print(str(ver))
+    assert str(ver) == '1.2.0'
