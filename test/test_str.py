a24f319386e8972c39cb202fa7e58fdc12588778
Esben Sonne <esbensonne@gmail.com>
2014-05-09 15:22:31 +0200

    Added test for correct str().

--- test/test_str.py
+++ test/test_str.py
@@ -5,3 +5,9 @@ def test_same():
     ver = SemVerPy('1.0.1-a')
     print(str(ver))
     assert str(ver) == '1.0.1-a'
+
+
+def test_dependency():
+    ver = SemVerPy('1.2')
+    print(str(ver))
+    assert str(ver) == '1.2.x'
