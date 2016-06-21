10f6547aa23a37efbc2e17f0126fe34fe7acd095
Jeremy Zafran <jeremy.zafran@cloudlock.com>
2016-06-21 19:37:20 -0400

    added classifiers to setup.py for compatibility with tools like caniusepython3

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 from semverpy import __version__, __author__, __license__
 
+
+classifiers = [
+    'Programming Language :: Python :: {}'.format(v)
+    for v in (2, 2.7, 3, 3.3, 3.4, 3.5,)]
+
+
 setup(
     name='SemVerPy',
     description='Bump versions. Semantically.',
@@ -9,5 +15,6 @@ setup(
     author_email='k@mackwerk.dk',
     license=__license__,
     url='https://github.com/Dinoshauer/SemVerPy',
-    py_modules=['semverpy']
+    py_modules=['semverpy'],
+    classifiers=classifiers
 )
